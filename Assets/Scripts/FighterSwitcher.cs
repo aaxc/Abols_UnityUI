664920efde1d7db1274680d0bfdba459a155de35
Dainis Abols <dainis@dainisabols.lv>
2026-02-20 20:11:17 +0200

    Add dragg and drop and additional sound effects

--- Assets/Scripts/FighterSwitcher.cs
+++ Assets/Scripts/FighterSwitcher.cs
@@ -6,16 +6,25 @@ public class FighterSwitcher : MonoBehaviour
     [SerializeField] private Image targetImage;
     [SerializeField] private Sprite robotSprite;
     [SerializeField] private Sprite humanSprite;
-    
+
+    private SFXScript sfxScript;
+
+    private void Start()
+    {
+        sfxScript = FindFirstObjectByType<SFXScript>();
+    }
+
     public void OnDropdownChanged(int index)
     {
         if (index == 0)
         {
+            sfxScript.PlaySFX(1);
             targetImage.sprite = robotSprite;
         }
 
         if (index == 1)
         {
+            sfxScript.PlaySFX(2);
             targetImage.sprite = humanSprite;
         }
     }
