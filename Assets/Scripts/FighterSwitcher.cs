44db774cf5f585eff0f2786ae47371269e200440
Dainis Abols <dainis@dainisabols.lv>
2026-02-20 21:51:15 +0200

    Add scroll view text swap

--- Assets/Scripts/FighterSwitcher.cs
+++ Assets/Scripts/FighterSwitcher.cs
@@ -7,6 +7,9 @@ public class FighterSwitcher : MonoBehaviour
     [SerializeField] private Sprite robotSprite;
     [SerializeField] private Sprite humanSprite;
 
+    [SerializeField] private GameObject contentRobot;
+    [SerializeField] private GameObject contentHuman;
+
     private SFXScript sfxScript;
 
     private void Start()
@@ -20,12 +23,18 @@ public class FighterSwitcher : MonoBehaviour
         {
             sfxScript.PlaySFX(1);
             targetImage.sprite = robotSprite;
+
+            contentRobot.SetActive(true);
+            contentHuman.SetActive(false);
         }
 
         if (index == 1)
         {
             sfxScript.PlaySFX(2);
             targetImage.sprite = humanSprite;
+
+            contentHuman.SetActive(true);
+            contentRobot.SetActive(false);
         }
     }
 }
