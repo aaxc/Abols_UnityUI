e84ed9aa3b68fe111e26ebb0d2719592206eba7c
Dainis Abols <dainis@dainisabols.lv>
2026-02-20 21:04:44 +0200

    Add minor tweaks

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+    [SerializeField] private Vector2 hotspot = Vector2.zero;
+
+    private void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotspot, CursorMode.Auto);
+    }
+}
